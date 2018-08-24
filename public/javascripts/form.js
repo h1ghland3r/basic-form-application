cda0f3467ef04270843074cd6d53fd6f33f83fff
Railan Barbosa <railan.barbosa@osf-commerce.com>
2018-08-23 23:53:13 -0300

    Add e-mail validation when its exists

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,25 +1,35 @@
 $(document).ready(function(){
     $form = $("#formSubmit");
-    
+
     $form.submit (function (e) {
         e.preventDefault();
-        validateBeforeSubmit();
+        initValidate();
     });
 
-    function validateBeforeSubmit() {
-        var formData = JSON.stringify($form.serializeArray());
-    
+    function initValidate() {
+        var formData = {};
+        formData.name = $form.find("input[name='name']").val();
+        formData.email = $form.find("input[name='email']").val();
+        formData.password = $form.find("input[name='password']").val();
+        formData.phone = $form.find("input[name='phone']").val();
+        $("#validationSuccess").html('');
+        $("#validationError").html('');
+
         $.ajax({
             type: "POST",
             dataType: "json",
             contentType: "application/json",
-            data: formData,
-            url: "/add",
-            success: function () {
-                alert(data);
+            data: JSON.stringify(formData),
+            url: "http://localhost:3000/add",
+            success: function (data) {
+                alert('All done! We received your data.');
+                if (typeof data.redirect == 'string') {
+                    // uses window.location.replace for not keeping the history navigation
+                    window.location.replace(window.location.protocol + "//" + window.location.host + data.redirect);
+                }
             },
             error: function (err) {
-                alert ('error');
+                $("#validationError").addClass('error-message').append('The e-mail ' + formData.email + ' is already registered.');
             }
         });
     }
